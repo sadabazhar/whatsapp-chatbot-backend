f7e45cb054568543e1439095f508cd37ee44fa10
SADAB AZHAR <sadabazhar84@gmail.com>
2026-04-27 14:35:41 +0530

    Initial project setup using Spring Boot for WhatsApp chatbot webhook

---
+++ src/test/java/com/jaruratcare/whatsappchatbot/WhatsappChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaruratcare.whatsappchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
